b9fd2e960c8990f77c4875e56b0a35049748e41c
Anders Hagward <taperlawyer@gmail.com>
2018-08-31 16:35:03 +0200

    Add real model

--- src/Main.elm
+++ src/Main.elm
@@ -1,29 +1,63 @@
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as Decode
+import Url.Builder as Url
 
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
 
-type Msg = SelectRow Int
+type alias Track =
+  { creator : String
+  , title : String
+  , location : String
+  }
 
+type alias Model =
+  { selected : Int
+  , tracks : List Track
+  }
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( Model 0 [ (Track "creator" "title" "location"), (Track "creator2" "title2" "location") ]
+  , Cmd.none
+  )
+
+type Msg = SelectTrack Int
+
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    SelectRow a ->
-      a
+    SelectTrack track ->
+      ( { model | selected = track }
+      , Cmd.none
+      )
 
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+view : Model -> Html Msg
 view model =
-  div []
-    [ row model 1 "One" SelectRow
-    , row model 2 "Two" SelectRow
-    , row model 3 "Three" SelectRow
-    , row model 4 "Four" SelectRow
-    , row model 5 "Five" SelectRow
-    ]
+  div [] (rows model)
 
-row model n t select =
-  if model == n then
-    div [ style "color" "green", onClick (select n) ] [ text t ]
+rows : Model -> List (Html msg)
+rows model =
+  model.tracks
+    |> List.indexedMap Tuple.pair
+    |> List.map (\(i, t) -> (i, t, model.selected))
+    |> List.map row
+
+row (i, track, selected) =
+  if selected == i then
+    div [ style "color" "green" ] [ text (track.creator ++ " - " ++ track.title) ]
   else
-    div [ onClick (select n) ] [ text t ]
+    div [] [ text (track.creator ++ " - " ++ track.title) ]
