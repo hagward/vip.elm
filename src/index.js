70231d5ea3f751a3787ef1ec4e08636f236ffa25
Anders Hagward <taperlawyer@gmail.com>
2020-04-26 10:57:00 +0200

    Remove service worker

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-import "./style.css";
 import { Elm } from "./Main.elm";
-import registerServiceWorker from "./registerServiceWorker";
+import "./style.css";
 
 const app = Elm.Main.init({
-  node: document.getElementById("elm-root")
+  node: document.getElementById("elm-root"),
 });
 
 const audio = () => document.getElementById("audio");
 
-app.ports.scrollToTrack.subscribe(index => {
+app.ports.scrollToTrack.subscribe((index) => {
   const track = document.querySelectorAll("#playlist li")[index];
   if (track) {
     track.scrollIntoView();
   }
 });
-app.ports.seek.subscribe(time => (audio().currentTime = time));
-app.ports.playPause.subscribe(play => play ? audio().play().then() : audio().pause());
-
-registerServiceWorker();
+app.ports.seek.subscribe((time) => (audio().currentTime = time));
+app.ports.playPause.subscribe((play) =>
+  play ? audio().play().then() : audio().pause()
+);
